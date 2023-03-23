6abf3bcb6ddf6ba6f3c0f33dd8be3a5ce2a8d0ec
rithaven <veneruth4@gmail.com>
2023-03-23 16:35:15 +0200

    remove bugs from dev branch

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-
-
+import { useState, useEffect } from "react"
 export default function Meme() {
-    const [meme, setMeme] = React.useState({
+    const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemes, setAllMemes] = React.useState([])
+    const [allMemes, setAllMemes] = useState([])
     
-    React.useEffect(() => {
+    useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
             .then(data => setAllMemes(data.data.memes))
